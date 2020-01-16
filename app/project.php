0263d8fc99eb06b36e55ac6eab954e7d7d13e3cf
anmol <anmolsingh0608@gmail.com>
2020-01-16 12:09:24 +0530

    testing

---
+++ app/project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class project extends Model
+{
+    public $fillable = ['pname','description'];
+
+    public function technologies()
+    {
+    	return $this->belongstoMany('App\technology');
+    }
+
+    public function projectsToUser()
+    {
+        return $this->belongstoMany('App\User');
+    }
+}
